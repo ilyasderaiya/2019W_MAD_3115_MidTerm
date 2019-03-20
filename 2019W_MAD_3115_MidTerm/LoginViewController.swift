04dab97d52af7235f7674e7ece9e31ac1d1caa04
ilyas <ilyasderaiya@gmail.com>
2019-03-20 19:29:39 -0400

    Added login Logic And UI to Student Entry VC

--- 2019W_MAD_3115_MidTerm/LoginViewController.swift
+++ 2019W_MAD_3115_MidTerm/LoginViewController.swift
@@ -17,8 +17,70 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let userDefault = UserDefaults.standard
+        
+        if let email = userDefault.string(forKey: "userEmail")
+        {
+            txtEmail.text = email
+            if let pwd = userDefault.string(forKey: "userPassword")
+            {
+                txtPassword.text = pwd
+            }
+            
+            swRememberMe.isOn = true
+        }
+        else
+        {
+            swRememberMe.isOn = false
+        }
     }
 
+    @IBAction func btnSignInClick(_ sender: Any) {
+        let email = txtEmail.text
+        let pwd = txtPassword.text
+        if ((txtEmail.text?.isEmpty)!) || ((txtPassword.text?.isEmpty)!) {
+            //Alert can't be Empty
+            displayMessage(userMessage: "Fields Can't be Empty")
+            return
+        }
+        
+        
+        if (email == "admin@admin.com" && pwd == "admin@123"){
+            let userDefault = UserDefaults.standard
+            if(swRememberMe.isOn)
+            {
+                //Store Data to User Default Storage
+                userDefault.set(email, forKey: "userEmail")
+                userDefault.set(pwd, forKey: "userPassword")
+                
+            }else
+            {
+               //Remove UserId/Password if previously remembered/stored
+                userDefault.removeObject(forKey: "userEmail")
+                userDefault.removeObject(forKey: "userPassword")
+            }
+            
+            let sb = UIStoryboard(name: "Main", bundle: nil)
+            let studVC = sb.instantiateViewController(withIdentifier: "studVC") as! StudentEntryViewController
+            //self.navigationController?.pushViewController(studVC, animated: true)
+            self.present(studVC, animated: true)
+           
+            
+        }else{
+            displayMessage(userMessage: "Invalid Email or Password")
+        }
+        
+        
+    }
+    
+    
+    func displayMessage(userMessage: String) -> Void {
+            let alert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: .alert)
+            
+            let OKAction = UIAlertAction(title: "OK", style: .default){ (action:UIAlertAction!) in }
+            alert.addAction(OKAction)
+            self.present(alert, animated: true)
+    }
 
 }
 
--- 2019W_MAD_3115_MidTerm/Students/StudentEntryViewController.swift
+++ 2019W_MAD_3115_MidTerm/Students/StudentEntryViewController.swift
@@ -8,23 +8,46 @@
 
 import UIKit
 
-class StudentEntryViewController: UIViewController {
+class StudentEntryViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
 
+    @IBOutlet var txtStudId: UIView!
+    @IBOutlet weak var txtStudName: UITextField!
+    @IBOutlet weak var txtStudGender: UITextField!
+    @IBOutlet weak var txtStudCourse: UITextField!
+    @IBOutlet weak var txtStudEmail: UITextField!
+    @IBOutlet weak var txtStudBirthDate: UITextField!
+    @IBOutlet weak var txtStudSub1: UITextField!
+    @IBOutlet weak var txtStudSub2: UITextField!
+    @IBOutlet weak var txtStudSub3: UITextField!
+    @IBOutlet weak var txtStudSub4: UITextField!
+    @IBOutlet weak var txtStudSub5: UITextField!
+    var myPickerView : UIPickerView!
+    var datePicker : UIDatePicker!
+    var courseList = ["MADT", "MODT", "CSD", "WADT"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return courseList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return courseList[row]
+    }
+
+   
 
 }
